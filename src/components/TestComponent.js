95c8b7419bdecb4fa5678e0bdd2ef52da4abfddc
DrkSephy <dleonar00@citymail.cuny.edu>
2017-11-07 19:57:19 -0500

    Adding base react project

---
+++ src/components/TestComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TestComponent extends Component {
+  render () {
+    return (
+      <h1>This is a test component!</h1>
+    )
+  }
+}
+
+export default TestComponent
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TestComponent from './components/TestComponent';
+
+console.log(TestComponent);
+
+ReactDOM.render(
+  <TestComponent />,
+  document.getElementById('root')
+);
